7de9cd322835e9c6cad75a5ca05bc55fe8c4cc58
selva-rajT <selvaraj@centizen.com>
2023-06-20 17:32:10 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
-
+import { useEffect ,useState} from 'react';
 function App() {
+  const [pin,setPin]=useState(0);
+  const [data,setData]=useState([]);
+  useEffect(()=>{
+    fetchData();
+  },[])
+  const fetchData=async()=>{
+    try{
+      const response=await axios.get('https://flamesbackend.onrender.com/user/');
+      setData(response.data);
+    }
+    catch(er){
+      console.log(er);
+    }
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pin!=2428?<label htmlFor='pin'>Pin</label>:null}
+      {pin!=2428?<input type='number' name='pin' onChange={(e)=>setPin(e.target.value)}></input>:null}
+      {pin==2428?<table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name 1</th>
+          <th>Name 2</th>
+          <th>Result</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name1}</td>
+            <td>{item.name2}</td>
+            <td>{item.result}</td>
+          </tr>
+        ))}
+      </tbody>
+      </table>:null}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+  
+
+
